048a97161ad352b87cf12dee8e5b9335474c14bd
GLXALOKESH <alokeshmaitra9@gmail.com>
2025-06-28 16:47:21 +0530

    setup done

---
+++ Client/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
